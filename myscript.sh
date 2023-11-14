5a187bc568a38bbf05201f02ed210e637d8376bf
dyparkkk-lg <dyparkkk@lguplus.co.kr>
2023-11-14 20:50:32 +0900

    myscript.sh 컨테이너 이름 변경

--- myscript.sh
+++ myscript.sh
@@ -3,4 +3,4 @@
 docker stop lottery-team
 docker rm lottery-team
 docker build . -t lottery-team
-docker run -d --name lottery -p 8889:8889 lottery-team
+docker run -d --name lottery-team -p 8889:8889 lottery-team
